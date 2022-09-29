adc45a29b1812d0c1f102db643e2145f83da8495
Hosamov <hosamov@hotmail.com>
2022-09-29 09:20:47 -0700

    add routes, dummy data, and images

---
+++ pages/events/[...slug].js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+function FilteredEventsPage() {
+
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
