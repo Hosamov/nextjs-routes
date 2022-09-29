ea98e4ccb75d3dd87b3797738a049d352547e1d1
Hosamov <hosamov@hotmail.com>
2022-09-29 11:05:36 -0700

    add search bar

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,18 +1,22 @@
 import Link from 'next/link';
 
+import classes from './main-header.module.css';
+
 function MainHeader() {
-  return <header>
-    <div>
-      <Link href='/'>NextEvents</Link>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link href='/events'>All Events</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">NextEvents</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainHeader;
